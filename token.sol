129ba561838b4d2bff175d475199c6ca129d91e1
R-Rozkov <137500276+R-Rozkov@users.noreply.github.com>
2023-06-25 23:18:11 +0300

    Create token.sol

---
+++ token.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.5.0;
+
+contract Token {
+    string  public name = "DApp Token";
+    string  public symbol = "DAPP";
+    uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens
+    uint8   public decimals = 18;
+
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
+    
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+}
